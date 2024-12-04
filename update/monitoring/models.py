74bc98efc7968a516ab6b67b0332cb960be03dca
vineethkumar0812 <vineethkumardec8@gmail.com>
2024-12-03 21:46:40 -0500

    Final files

---
+++ update/monitoring/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_user = models.BooleanField(default=False)
+
+    class Meta:
+        swappable = 'AUTH_USER_MODEL'
